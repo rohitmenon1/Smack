9734954af3c34f32b02cefea159e673ef291d3d9
Louis Régis <louisregis@Louiss-MacBook-Pro.local>
2018-03-07 13:55:27 +0100

    Login VC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Louis Régis on 06.03.18.
+//  Copyright © 2018 Hospinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Smack
+//
+//  Created by Louis Régis on 06.03.18.
+//  Copyright © 2018 Hospinn. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+
+let TO_LOGIN = "toLogin"
