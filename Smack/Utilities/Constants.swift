a0167c0007e592f3a1c46320220375e514321230
Louis Régis <louisregis@Louiss-MacBook-Pro.local>
2018-03-09 14:46:35 +0100

    Auth Registration User

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,9 +8,22 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ _Success: Bool) -> ()
+
+// URL Constants
+
+let BASE_URL = " https://chattychat123456.herokuapp.com/"
+let URL_REGISTER= "\(BASE_URL)account/register"
 
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
